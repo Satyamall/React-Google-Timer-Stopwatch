7065580c590d7cdb919b71c09d98b8d3d0e37821
Satyamall <thesatyamall123@gmail.com>
2021-12-02 14:55:14 +0530

    Satya,React code of Timer and Stopwatch

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 
+import { useState } from 'react';
 import './App.css';
 import StopWatch from './Components/Stopwatch';
 import Timer from './Components/Timer';
 
 function App() {
+  const [display, setDisplay] = useState(true);
   return (
+    <>
+    <h1 className="h-tag">Timer And Stopwatch</h1>
     <div className="App">
-      <div className="timer">
-        <Timer/>
+      <div className="button-box">
+        <button onClick={() => setDisplay(true)}>Timer</button>
+        <button onClick={() => setDisplay(false)}>Stopwatch</button>
       </div>
-      <div className="stopwatch">
-        <StopWatch/>
-      </div>
+      {
+        display ?
+          <div className="timer">
+            <Timer />
+          </div> :
+          <div className="stopwatch">
+            <StopWatch />
+          </div>
+      }
     </div>
+    </>
   );
 }
 
